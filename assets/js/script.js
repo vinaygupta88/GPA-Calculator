dd0bdd8582c21d2d8b7dff3f548c896ae8316107
Vinay Kumar <vinayku73094@gmail.com>
2025-08-20 21:42:07 +0530

    Scripts for email verification

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', e => {
     const href = link.getAttribute('href');
 
-    // Check if it's an internal anchor link (starts with #)
     if (href.startsWith('#')) {
       e.preventDefault();
       document.querySelector(href).scrollIntoView({
@@ -20,3 +19,23 @@ document.querySelectorAll('nav a').forEach(link => {
     document.getElementById('nav-links').classList.remove('show');
   });
 });
+
+// email validation
+function validateGmail() {
+    let emailInput = document.getElementById("email").value.trim();
+    let gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+    let message = document.getElementById("message");
+
+    if (emailInput === "") {
+        message.innerHTML = "Email is required!";
+        return false; 
+    } 
+    else if (!gmailRegex.test(emailInput)) {
+        message.innerHTML = "Please enter a valid Gmail address (e.g., example@gmail.com)";
+        return false; 
+    } 
+    else {
+        message.innerHTML = ""; 
+        return true;  
+    }
+}
